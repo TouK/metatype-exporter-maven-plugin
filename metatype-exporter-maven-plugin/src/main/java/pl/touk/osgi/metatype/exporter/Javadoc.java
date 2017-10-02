b65f5d34d5a0ae55a2896cbeb787913f1c44831a
Piotr Fus <pfs@touk.pl>
2017-10-02 10:19:12 +0200

    Add javadocs and sources plugin
    Change-Id: Ifee295f9c9f053b62a9d0601aaae72e131a30572

---
+++ metatype-exporter-maven-plugin/src/main/java/pl/touk/osgi/metatype/exporter/Javadoc.java
@@ -0,0 +1,7 @@
+package pl.touk.osgi.metatype.exporter;
+
+/**
+ * @see <a href="https://touk.pl/blog/2017/08/25/tired-of-exporting-your-osgi-metatype-to-client-manually/">Link</a>.
+ */
+public class Javadoc {
+}
